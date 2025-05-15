f2037ced7c69c35b458e298ebd772acd736c81df
Anthony Nguyen <46362634+AnthonyN3@users.noreply.github.com>
2025-05-15 02:08:47 -0400

    Refactor to dynamically pre-loading images instead of loading all photos in memory (#10)
    * Refactor to use a buffering system to preload photos
    
    * cleanup
    
    * rename variable name queues to deque

---
+++ slideshow_project/main.py
@@ -0,0 +1,312 @@
+from collections import deque
+import platform
+import random
+from threading import Thread, Lock
+from tkinter import Tk, Label, constants as tkinter_constants
+from PIL import Image, ImageTk, ImageGrab, ImageOps
+import settings, helper
+
+# Constants
+BUFFER_SIZE = 5
+
+# Variables
+photo_dir_path: str
+img_file_names: list
+next_img_deque: deque
+prev_img_deque: deque
+forward_buffer_index: int
+backward_buffer_index: int = -1
+
+app_settings: settings.SlideSettings
+screen_size: None # tuple(int, int)
+current_img = None
+current_tk_photo = None
+isPaused = False
+isFullscreen: bool = True
+
+# Thread Locks used for preloading images on diff threads
+next_img_deque_lock = Lock()
+prev_img_deque_lock = Lock()
+
+# ***************************************
+# KEY BINDS
+# ***************************************
+
+def exit_slideshow(event=None):
+    stop_slideshow()
+    tk_window.destroy()
+
+def fullscreen(event=None):
+	global isFullscreen
+	tk_window.attributes('-fullscreen', not isFullscreen)
+	isFullscreen = not isFullscreen
+
+def pause_slideshow(event=None):
+    global isPaused, next_img_task_id
+    if isPaused:
+        isPaused = False
+        tk_label.config(text="")
+        next_img_task_id = tk_window.after(app_settings.delay_ms, schedule_next_photo)
+    else:
+        isPaused = True
+        stop_slideshow()
+        tk_label.config(text="PAUSED")
+
+def speedup_slideshow(event=None):
+	if not isPaused:
+		if(app_settings.delay_ms > settings.MIN_DELAY_MS):
+			app_settings.delay_ms = app_settings.delay_ms - 500
+			display_speed(app_settings.delay_ms )
+			reset_timer()
+
+def slowdown_slideshow(event=None):
+	if not isPaused:
+		if(app_settings.delay_ms  < settings.MAX_DELAY_MS):
+			app_settings.delay_ms  = app_settings.delay_ms  + 500
+			display_speed(app_settings.delay_ms )
+			reset_timer()
+               
+def display_speed(speed_ms):
+    global text_task_id
+    if "text_task_id" in globals():
+        tk_window.after_cancel(text_task_id)
+    tk_label.config(text="\n\n\n\n\n" + helper.ms_to_sec(speed_ms) + " seconds")
+    text_task_id = tk_window.after(1000, remove_text)
+     
+def remove_text():
+	tk_label.config(text="")
+     
+def reset_timer():
+    global next_img_task_id
+    if "next_img_task_id" in globals():
+        tk_window.after_cancel(next_img_task_id)
+    next_img_task_id = tk_window.after(app_settings.delay_ms, schedule_next_photo)
+
+def next_photo(event=None):
+    global next_img_task_id, current_img, current_tk_photo, backward_buffer_index
+
+    # Check if deque is not empty
+    if next_img_deque:
+        temp_img = current_img
+
+        current_img = next_img_deque.popleft()
+        current_tk_photo = ImageTk.PhotoImage(current_img)
+
+        tk_label.config(image=current_tk_photo)
+
+        if temp_img:
+            with prev_img_deque_lock:
+                if (len(prev_img_deque) == prev_img_deque.maxlen):
+                    prev_img_deque.popleft().close()
+                    backward_buffer_index = backward_buffer_index + 1
+                prev_img_deque.append(temp_img)
+        
+        # Call method asynchronously here to open a new image and append it to next_img_deque
+        Thread(target=preload_next_image, daemon=True).start()
+          
+def previous_photo(event=None):
+    global next_img_task_id, current_img, current_tk_photo, forward_buffer_index
+
+    # Check if deque is not empty
+    if prev_img_deque:
+        temp_img = current_img
+
+        current_img = prev_img_deque.pop()
+        current_tk_photo = ImageTk.PhotoImage(current_img)
+
+        tk_label.config(image=current_tk_photo)
+
+        if temp_img:
+            with next_img_deque_lock:
+                if (len(next_img_deque) == next_img_deque.maxlen):
+                    next_img_deque.pop().close()
+                    forward_buffer_index = forward_buffer_index - 1
+
+                next_img_deque.appendleft(temp_img)
+
+        Thread(target=preload_previous_image, daemon=True).start()
+
+def print_info(event=None):
+    print("***************")
+    print("***************")
+    print()
+    print("img_file_names size:", len(img_file_names))
+    print("img_file_names:", img_file_names)
+    print()
+    print("frwd_buf_index:", forward_buffer_index)
+    print("bkwrd_buf_index:", backward_buffer_index)
+    print()
+    print("prev_q", len(prev_img_deque), [obj.filename for obj in prev_img_deque])
+    print("curr_img:", current_img.filename)
+    print("next_q", len(next_img_deque), [obj.filename for obj in next_img_deque])
+    print()
+    print("***************")
+    print("***************")
+
+# ***************************************
+# Functions that run in background thread
+# ***************************************
+def preload_next_image():
+    global forward_buffer_index, backward_buffer_index
+    try:
+        with next_img_deque_lock:
+
+            # Reset Indexes when reaching the end of photo set
+            if forward_buffer_index >= len(img_file_names):
+                forward_buffer_index = 0
+                with prev_img_deque_lock:
+                    backward_buffer_index = -(BUFFER_SIZE*2+1) # this is to account for photos already in prev buffer deque
+                if app_settings.isRandomized:
+                     random.shuffle(img_file_names)
+
+            img = Image.open(fp = photo_dir_path + img_file_names[forward_buffer_index], formats=settings.VALID_FORMATS)
+
+            if False if img.getexif().get(0x0112) == 1 else True:
+                img = ImageOps.exif_transpose(img)
+            if app_settings.isCropped:
+                img = ImageOps.fit(image=img, size=screen_size, method=Image.Resampling.LANCZOS)
+            else:
+                img = ImageOps.contain(image=img, size=screen_size, method=Image.Resampling.LANCZOS)
+            img.filename = img_file_names[forward_buffer_index]
+
+            # Add Next Photo
+            next_img_deque.append(img)
+            forward_buffer_index = forward_buffer_index + 1
+    except Exception as e:
+        print(f"Error Loading Next Photo: {e}")
+
+def preload_previous_image():
+    global backward_buffer_index
+    try:
+        with prev_img_deque_lock:
+            if (backward_buffer_index > -1):
+                img = Image.open(fp = photo_dir_path + img_file_names[backward_buffer_index], formats=settings.VALID_FORMATS)
+
+                if False if img.getexif().get(0x0112) == 1 else True:
+                    img = ImageOps.exif_transpose(img)
+                if app_settings.isCropped:
+                    img = ImageOps.fit(image=img, size=screen_size, method=Image.Resampling.LANCZOS)
+                else:
+                    img = ImageOps.contain(image=img, size=screen_size, method=Image.Resampling.LANCZOS)
+                img.filename = img_file_names[backward_buffer_index]
+
+                # Add Previous Photo
+                prev_img_deque.appendleft(img)
+                backward_buffer_index = backward_buffer_index - 1
+    except Exception as e:
+        print(f"Error Loading Previous Photo: {e}")
+
+# ***************************************
+
+def stop_slideshow():
+    tk_window.after_cancel(next_img_task_id)
+
+def schedule_next_photo():
+    global next_img_task_id, current_img, current_tk_photo, backward_buffer_index
+
+    temp_img = current_img
+
+    current_img = next_img_deque.popleft()
+    current_tk_photo = ImageTk.PhotoImage(current_img)
+
+    tk_label.config(image=current_tk_photo)
+
+    if temp_img:
+        with prev_img_deque_lock:
+            if (len(prev_img_deque) == prev_img_deque.maxlen):
+                prev_img_deque.popleft().close()
+                backward_buffer_index = backward_buffer_index + 1
+                # ^ TODO: This doesn't work once you to a full lap around the img_file_names and want to reset the indexes.
+            prev_img_deque.append(temp_img)
+    
+    # Call method asynchronously here to open a new image and append it to next_img_deque
+    Thread(target=preload_next_image, daemon=True).start()
+
+    next_img_task_id = tk_window.after(app_settings.delay_ms, schedule_next_photo)
+
+def load_initial_photos():
+    global forward_buffer_index
+    print("\n Attempting To Load Initial Photos")
+    for index, file_name in enumerate(img_file_names):
+        try:
+
+            forward_buffer_index = index
+            if(len(next_img_deque) == next_img_deque.maxlen):
+                break
+
+            img = Image.open(fp = photo_dir_path + file_name, formats=settings.VALID_FORMATS)
+
+            # Checks if the orientation tag present isn't 1 (no tranpose needed)
+            # I found checking for orientation tag saves a couple seconds rather than applying exif_transpose on all images and returning a copy
+            isTransposed = False if img.getexif().get(0x0112) == 1 else True # 2-8 means the image has a orientation tag that programs apply when viewing
+            if isTransposed:
+                img = ImageOps.exif_transpose(img) #  applies orientation transpose
+
+            if app_settings.isCropped:
+                img = ImageOps.fit(image=img, size=screen_size, method=Image.Resampling.LANCZOS)
+            else:
+                img = ImageOps.contain(image=img, size=screen_size, method=Image.Resampling.LANCZOS)
+
+            img.filename = file_name
+            next_img_deque.append(img)
+            print(" +[LOADED] -", file_name, ("(photo transposed)" if isTransposed else '') )
+
+        except Exception as e:
+            print(" -[FAILED] -", e)
+
+# ---- Run Everything ----
+if __name__ == "__main__":
+    helper.print_controls(settings.MIN_DELAY_MS, settings.MAX_DELAY_MS)
+
+    # Initialize deque and set the max length
+    next_img_deque = deque(maxlen=BUFFER_SIZE)
+    prev_img_deque = deque(maxlen=BUFFER_SIZE)
+
+    # Grab slideshow settings from cmd line options and or user input
+    app_settings = settings.get_cmd_line_options()
+    settings.get_user_input(app_settings)
+
+    # Set photo path and get list of photo file names
+    photo_dir_path = helper.get_photo_path()
+    img_file_names = helper.get_photo_file_names(photo_dir_path, settings.VALID_FILE_EXT)
+
+    if app_settings.isRandomized:
+        random.shuffle(img_file_names)
+    else:
+        img_file_names = sorted(img_file_names)
+
+    screen_size = ImageGrab.grab().size
+    load_initial_photos()
+
+    app_settings.print_values()
+
+    # Initialize a display window
+    tk_window = Tk()
+    tk_window.title("Simpler SlideShow")
+    tk_window.attributes('-fullscreen', isFullscreen)
+    tk_window.config(cursor="none", bg=app_settings.bg_color)
+    tk_window.focus_force()
+
+    # creates/pack label widget onto the root window
+    tk_label = Label(
+        tk_window,
+        anchor=tkinter_constants.CENTER,
+        borderwidth="0",
+        compound=tkinter_constants.CENTER,
+        font=('Arial' if platform.system() == 'Windows' else 'Liberation Mono',50),
+        fg='#ef0000'
+        )
+    tk_label.pack()
+
+    # Binding keys to an event/method
+    tk_window.bind("<Escape>", exit_slideshow)
+    tk_window.bind("<space>", pause_slideshow)
+    tk_window.bind("<Down>", slowdown_slideshow)
+    tk_window.bind("<Up>", speedup_slideshow)
+    tk_window.bind("<Left>", previous_photo)
+    tk_window.bind("<Right>", next_photo)
+    tk_window.bind("<f>", fullscreen)
+    tk_window.bind("<i>", print_info)
+
+    schedule_next_photo()
+    tk_window.mainloop()
---
+++ slideshow_project/settings.py
@@ -0,0 +1,142 @@
+import sys, getopt, re
+import helper
+
+MAX_DELAY_MS = 30_000
+MIN_DELAY_MS = 500
+
+VALID_FILE_EXT = (".jpeg", ".jpg", ".png", ".tga", ".webp", ".bmp", ".psd")
+VALID_FORMATS = ("JPEG", "PNG", "TGA", "WEBP", "BMP", "PSD")
+
+YES_ANS = ("yes", "y", "ya", "ye", "yee", "yup", "yuh", "yeah", "okay", "ok", "yep", "yea", "alright", "roger", "oui", "sure")
+BG_COLOR_OPTIONS = ("white", "black", "red", "green", "blue", "cyan", "yellow", "magenta")
+
+class SlideSettings:
+    def __init__(self, isRandomized: bool, isCropped: bool, delay_ms: int, bg_color: str):
+        self.isRandomized = isRandomized
+        self.isCropped = isCropped
+        self.delay_ms = delay_ms
+        self.bg_color = bg_color
+    
+    def print_values(self):
+        print("\n RESIZING = ENABLED")
+        print(" CROPPING = " + ("ENABLED" if self.isCropped else "DISABLED" ))
+        print(" RANDOMIZATION = " + ("ENABLED" if self.isRandomized else "DISABLED" ))
+        print(" DELAY = " + str(round(self.delay_ms/1000, 1)) +" sec")
+        if self.bg_color:
+            print(" BG COLOR = " + self.bg_color)
+        print("\n")
+            
+
+def get_cmd_line_options() -> SlideSettings:
+    args = sys.argv[1:]
+    if args:
+        try:
+            option_value, arg_val = getopt.getopt(
+                args, 
+                "hrocfb:t:", 
+                ["help","randomize", "order", "crop", "fit", "bg-color=", "timer="]
+                )
+
+            if arg_val:
+                print("Illegal arguments:", arg_val)
+                print(f"Type {sys.argv[0]} --help to see a list of options")
+                sys.exit()
+
+            # Error message if certain options are used together
+            i = [x[0] for x in option_value]
+            if ("-c" in i or "--crop" in i) and ("-f" in i or "--fit" in i):
+                print("Cannot use option \"--crop\" and \"--fit\" together")
+                print(f"Type {sys.argv[0]} --help to see a list of options")
+                sys.exit()
+            if ("-r" in i or "--randomize" in i) and ("-o" in i or "--order" in i):
+                print("Cannot use option \"--randomize\" and \"--order\" together")
+                print(f"Type {sys.argv[0]} --help to see a list of options")
+                sys.exit()
+
+            isRandomize = None
+            isCropped = None
+            delay_ms = None
+            bg_color = None
+
+            for opt, val in option_value:
+                if opt in ("-h", "--help"):
+                    helper.print_help()
+                    sys.exit()
+                elif opt in ("-r", "--randomize"):
+                    isRandomize = True
+                elif opt in ("-o", "--order"):
+                    isRandomize = False
+                elif opt in ("-c", "--crop"):
+                    isCropped = True
+                elif opt in ("-f", "--fit"):
+                    isCropped = False
+                elif opt in ("-b", "--bg-color"):
+                    if val.lower() in BG_COLOR_OPTIONS:
+                        bg_color = val.lower()
+                    elif re.search("^([0-9a-fA-F]{3}){1,2}$", val):
+                        bg_color = '#' + val
+                    else:
+                        print("Invalid background color specified for --bg_color/-b")
+                        print(f"Type {sys.argv[0]} --help to see a list of options")
+                        sys.exit()
+                elif opt in ("-t", "--timer"):
+                    if helper.is_number(val):
+                        delay_input = int(round(float(val),1) * 1000) #convert second to ms
+                        if delay_input > MAX_DELAY_MS:
+                            print(f"Speed cannot be over {helper.ms_to_sec(MAX_DELAY_MS)} seconds")
+                            sys.exit()
+                        elif delay_input < MIN_DELAY_MS:
+                            print(f"Speed cannot be under {helper.ms_to_sec(MIN_DELAY_MS)} seconds")
+                            sys.exit()
+                        else:
+                            delay_ms = delay_input
+                    else:
+                        print("invalid value for --speed/-s")
+                        print(f"Type {sys.argv[0]} --help to see a list of options")
+                        sys.exit()
+
+            return SlideSettings(isRandomized=isRandomize, isCropped=isCropped, delay_ms=delay_ms, bg_color=bg_color)
+        
+        except getopt.error as e:
+            print(e)
+            print(f"\nUsage: {sys.argv[0]} [OPTIONS]")
+            print(f"Type {sys.argv[0]} --help to see a list of options")
+            sys.exit()
+
+    else:
+        return SlideSettings(isRandomized=None, isCropped=None, delay_ms=None, bg_color=None)
+
+def get_user_input(settings: SlideSettings):
+    if settings.delay_ms == None:
+        delay_input = input("\n Input slideshow speed in seconds: ")
+
+        # converts sec to ms and rounds down to nearest full second or half a second (0.5,1,1.5,etc)
+        if helper.is_number(delay_input):
+            # round one dec place and convert sec to ms 
+            delay_input = int(round(float(delay_input),1) * 1000)
+
+            if delay_input >= MAX_DELAY_MS:
+                delay_ms = MAX_DELAY_MS
+            elif delay_input <= MIN_DELAY_MS:
+                delay_ms = MIN_DELAY_MS
+            else:
+                rem = delay_input % 500
+                delay_ms = delay_input-rem if rem else delay_input
+            settings.delay_ms = delay_ms
+        else:
+            settings.delay_ms = 4500
+
+        print(" SPEED: " + str(round(settings.delay_ms/1000, 1)) +" sec")
+
+    if settings.isCropped == None:
+        settings.isCropped = input("\n Would you like the photos to be Cropped to fit? [y/n] ").lower() in YES_ANS
+        print(" CROP:", settings.isCropped)
+    if settings.isRandomized == None:
+        settings.isRandomized = input("\n Would you like photo sequence to be Randomized? [y/n] ").lower() in YES_ANS
+        print(" RANDOMIZED:", settings.isRandomized)
+    if not settings.isCropped and settings.bg_color == None:
+        bg_color_input = input(f"\n {BG_COLOR_OPTIONS} | or hex color (ex. #FF22AA)\n Input background color: ").lower()
+        if  (bg_color_input in BG_COLOR_OPTIONS) or (re.search("^#([0-9a-fA-F]{3}){1,2}$", bg_color_input)):
+            settings.bg_color = bg_color_input
+        else:
+            settings.bg_color = "black"
